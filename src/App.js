41a204027c3176be4a4740b1f106182a710fae32
Austin Davis <austindanieldavis@gmail.com>
2018-11-26 02:08:58 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  let element = <img src={logo}></img>;
+  return element;
+}
+
+export default App;
+ 
